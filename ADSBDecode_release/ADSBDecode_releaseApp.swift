36fd2469c02d521847d7fecbc837b495beed7e06
Arturs Artamonovs <arturs.artamonovs@protonmail.com>
2024-08-08 09:48:56 +0100

    Initial Commit

---
+++ ADSBDecode_release/ADSBDecode_releaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADSBDecode_releaseApp.swift
+//  ADSBDecode_release
+//
+//  Created by Jacky Jack on 08/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ADSBDecode_releaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
